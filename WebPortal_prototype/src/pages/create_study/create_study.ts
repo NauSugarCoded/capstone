7c85c119df240d288a4d518fb884948354520573
Alfonso Martinez <am3536@nau.edu>
2018-02-12 20:45:30 -0700

    Created study page layout

--- WebPortal_prototype/src/pages/create_study/create_study.ts
+++ WebPortal_prototype/src/pages/create_study/create_study.ts
@@ -7,8 +7,19 @@ import { NavController } from 'ionic-angular';
 })
 export class CreateStudyPage {
 
+  public study_fname: string;
+  public study_sname: string;
+  public study_sdate: string;
+  public study_edate: string;
+  public study_discription: string;
   constructor(public navCtrl: NavController) {
 
   }
+
+  submitStudy() {
+
+  }
+
+
 
 }
